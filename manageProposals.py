ce805e749e62d02e2be743463b3e4cfff1f60fcd
Jonathan Morgan <jtmorgan25@gmail.com>
2013-02-11 13:38:42 -0800

    clean up & comment

--- manageProposals.py
+++ manageProposals.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python
 
-# Copyright 2012 Jtmorgan
+# Copyright 2013 Jtmorgan
  
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -27,31 +27,34 @@ import pageData as p
 
 wiki = wikitools.Wiki(settings.apiurl)
 wiki.login(settings.username, settings.password)
+logging.basicConfig(filename='/home/jtmorgan/grantsbot/logs/proposals.log',level=logging.INFO)
 
-page_namespace = 'Grants:'
 
 conn = MySQLdb.connect(host = 'metawiki-p.userdb.toolserver.org', db = 'u_jtmorgan_p', read_default_file = '~/.my.cnf', use_unicode=1, charset="utf8" )
 cursor = conn.cursor()
 
-logging.basicConfig(filename='/home/jtmorgan/grantsbot/logs/proposals.log',level=logging.INFO)
 
-# curtime = str(datetime.utcnow())
+##GLOBAL VARIABLES##
+page_namespace = 'Grants:'
+curtime = str(datetime.utcnow())
 
-#updates revs table with new revs, proposal table with new proposals	
+
+##FUNCTIONS##
 def newProposals():	
+	#first, updates proposal activity table with new revs
 	cursor.execute('''
 INSERT IGNORE INTO ieg_proposal_edits (rev_id, rev_user, rev_user_text, rev_timestamp, rev_comment, page_id, page_title, page_namespace) SELECT rev_id, rev_user, rev_user_text, rev_timestamp, rev_comment, page_id, page_title, page_namespace FROM metawiki_p.page as p, metawiki_p.revision as r WHERE p.page_namespace IN (200, 201) AND p.page_title LIKE '%s' AND p.page_title NOT LIKE '%s' AND p.page_id = r.rev_page
 	''' % ("IEG/%", "IEG/%/%"))
 	conn.commit()	
 	rowsaffected = cursor.rowcount
 	if rowsaffected > 0:
-		#add in new proposal pages
+		#then, update proposal table with new proposals
 		cursor.execute('''
 INSERT IGNORE INTO ieg_proposals (page_id, page_title, p_created_date, p_latest_edit_date, p_creator_userid, pc_username) SELECT page_id, page_title, STR_TO_DATE(min, "%s"), STR_TO_DATE(max, "%s"), rev_user, rev_user_text FROM (SELECT MIN(rev_timestamp) as min, MAX(rev_timestamp) AS max, page_id, page_title, rev_timestamp, rev_user, rev_user_text FROM ieg_proposal_edits WHERE page_namespace = 200 GROUP BY page_id) AS tmp
 		''' % ("%Y%m%d%H%i%s", "%Y%m%d%H%i%s"))
 		conn.commit()
 		
-#updates status of proposals through categorylinks
+	#finally, updates status of proposals through categorylinks
 	cursor.execute('''UPDATE ieg_proposals AS p, (SELECT cl_from, cl_to FROM metawiki_p.categorylinks AS c, ieg_proposals AS pp WHERE c.cl_from = pp.page_id AND c.cl_to IN ("IEG/Proposals/Draft", "IEG/Proposals/Proposed", "IEG/Proposals/Ineligible", "IEG/Proposals/Withdrawn")) AS tmp
 	SET p.p_status = CASE tmp.cl_to 
 	WHEN "IEG/Proposals/Draft" THEN 'draft'
@@ -63,7 +66,8 @@ INSERT IGNORE INTO ieg_proposals (page_id, page_title, p_created_date, p_latest_
 	WHERE tmp.cl_from = p.page_id;''')
 	conn.commit()
 
-		
+
+#checks whether certain sections of a proposal have been completed. If so, removes a button template from the proposal page		
 def checkCompleteness():
 	prop_list = []
 	cursor.execute('SELECT page_id, page_title, max(part2), max(part3) FROM (SELECT e.page_id, p.page_title, (CASE WHEN e.rev_comment LIKE "/* Part 2:%" THEN 1 ELSE 0 END) AS part2, (CASE WHEN e.rev_comment LIKE "/* Part 3:%" THEN 1 ELSE 0 END) AS part3 from ieg_proposal_edits AS e, ieg_proposals AS p WHERE (e.rev_comment = "/* Part 3: Community Discussion */ new section" or e.rev_comment = "/* Part 2: The Project Plan */ new section") AND e.page_id = p.page_id AND p.p_status IN ("ineligible", "withdrawn", "draft", "proposed")) AS tmp GROUP BY page_id')
@@ -80,13 +84,13 @@ def checkCompleteness():
 			print text
 			page = wikitools.Page(wiki, page_namespace + item[1])
 			page_text = text
-	# 			page_text = page_text.encode('utf-8')
 			page.edit(page_text, summary = 'Removing button templates used to create new sections', bot = 1)		
 		else:
 			pass	
 	return prop_list
+
 	
-						
+#updates the completed status of proposals						
 def updateDB(list):
 		for item in list:
 			cursor.execute('''
@@ -94,15 +98,8 @@ def updateDB(list):
 			''' % (item[2], item[3], item[0]))
 			conn.commit()
 
-#writes a new line of data to the csv
-def writeline(data, writer):
-	try:
-# 		for i in range(len(message_list)):
-		writer.writerow( (data[0], data[1], data[2], data[3], data[4], data[5]) )
-	except ValueError:
-		writer.writerow( ("error!") )
 
-
+#creates an output csv file
 def writeReport():
 	f = open(sys.argv[1], 'wt')
 	writer = csv.writer(f, quoting=csv.QUOTE_NONNUMERIC)
@@ -112,51 +109,20 @@ def writeReport():
 	for row in rows:
 		ist = [row[0], row[1], row[2], row[3], row[4], row[5]]
 		writeline(ist, writer)
+		
+		
+#writes a new line of data to output csv
+def writeline(data, writer):
+	try:
+		writer.writerow( (data[0], data[1], data[2], data[3], data[4], data[5]) )
+	except ValueError:
+		writer.writerow( ("error!") )
 
 
 ##MAIN##
 newProposals()
 proposals = checkCompleteness()
-print proposals
 updateDB(proposals)
-# writeReport()
+writeReport()
 cursor.close()
 conn.close()
-
-#OLD VERSION, didn't use edit comments
-# def checkCompleteness():
-# 	prop_list = []
-# 	cursor.execute('SELECT page_id, page_title FROM ieg_proposals WHERE p_status IN ("ineligible", "withdrawn", "draft", "proposed") AND (p_has_sec2 = 0 OR p_has_sec3 = 0)')
-# 	rows = cursor.fetchall()
-# 	for row in rows:
-# 		prop_list.append([str(row[0]), row[1]])
-# 	print prop_list	
-# 	for item in prop_list:
-# 		part2 = False
-# 		part3 = False	
-# 		getSecs = p.Page(item[1], "Grants", 2)	
-# 		secs_ist = getSecs.getSectionData()
-# 		print secs_ist
-# 		text = ''
-# 		if "Scope:" in secs_ist:
-# 			part2 = True
-# 			item.append(1)
-# 		else:
-# 			item.append(0)	
-# 		if "Community Notification:" in secs_ist:
-# 			part3 = True	
-# 			item.append(1)
-# 		else:
-# 			item.append(0)
-# 		if part2 or part3:
-# # 			getText = p.Page(item[1], "Grants")
-# # 	print getText.title
-# 			text = getSecs.removeButtons(part2, part3)						
-# # 			print text
-# 			page = wikitools.Page(wiki, page_namespace + item[1])
-# 			page_text = text
-# # 			page_text = page_text.encode('utf-8')
-# 			page.edit(page_text, summary = 'Removing button templates used to create new sections', bot = 1)		
-# 		else:
-# 			pass	
-# 	return prop_list
--- messageSubmitters.py
+++ messageSubmitters.py
@@ -21,14 +21,12 @@ import settings
 from datetime import datetime
 import logging
 
-
 wiki = wikitools.Wiki(settings.apiurl)
 wiki.login(settings.username, settings.password)
-
+logging.basicConfig(filename='/home/jtmorgan/grantsbot/logs/reminders.log',level=logging.INFO)
 conn = MySQLdb.connect(host = 'metawiki-p.userdb.toolserver.org', db = 'u_jtmorgan_p', read_default_file = '~/.my.cnf', use_unicode=1, charset="utf8" )
 cursor = conn.cursor()
 
-logging.basicConfig(filename='/home/jtmorgan/grantsbot/logs/reminders.log',level=logging.INFO)
 
 ##GLOBAL VARIABLES##
 curtime = str(datetime.utcnow())
@@ -42,11 +40,9 @@ message_template = u'{{subst:Template:IEG/GrantsBot/Reminder|signature=~~~~}}'
 
 
 ##FUNCTIONS##
-
-#gets a list of today's editors to invite
+#gets a list of editor's to message
 def getUsernames(cursor):
 	cursor.execute('SELECT pc_username FROM ieg_proposals WHERE p_status = "draft" AND p_creator_userid != 0')
-# 	cursor.execute('SELECT user_name FROM metawiki_p.user WHERE user_name = "Jmorgan (WMF)"')
 	rows = cursor.fetchall()
 	if rows:
 		return rows
@@ -54,18 +50,17 @@ def getUsernames(cursor):
 		pass
 		
 
-#invites guests		
+#send the reminder message		
 def messageUsers():
 	for name in recipients:
 		page_title = page_namespace + name
 		page = wikitools.Page(wiki, page_title)
-		print message_template
-		print page
 		try:
 			page.edit(message_template, section="new", sectiontitle="== Individual Engagement Grant proposals due 15 February 2013 ==", summary="Automatic reminder to complete a submitted [[Grants:IEG|Individual Engagement Grant Proposal]]", bot=1)
 		except:
 			logging.info('REMIND: Reminder to User:' + name + ' failed at to send at ' + curtime)
 			continue
+
 
 ##MAIN##
 rows = getUsernames(cursor)
@@ -74,12 +69,9 @@ if rows:
 		name = row[0]
 		recipients.append(name)		
 	messageUsers()	
-	print recipients
-	logging.info('REMIND: Sent reminders to ' + ' '.join(recipients) + ' ' + curtime)
-	
+	logging.info('REMIND: Sent reminders to ' + ' '.join(recipients) + ' ' + curtime)	
 else:
 	logging.info('REMIND: No reminders today ' + curtime)
-
 cursor.close()
 conn.close()
 
