8a5b11f97e37afa15a969a438d069cc8e1baca1f
Jonathan Morgan <jtmorgan25@gmail.com>
2013-02-18 16:55:39 -0800

    activating csv output

--- scripts/manageProposals.py
+++ scripts/manageProposals.py
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import sys; sys.path.append('/home/jtmorgan/local/lib/python2.7/site-packages/')
+# import sys; sys.path.append('/home/jtmorgan/local/lib/python2.7/site-packages/')
 import wikitools
 import settings
 import MySQLdb
